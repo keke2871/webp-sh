b18967a77d1b020e6fa761e34ff3a09c47a1a8a1
wangyong <287144506@qq.com>
2024-08-14 23:33:34 +0800

    测试用例和图片集

--- handler/router_test.go
+++ handler/router_test.go
@@ -119,15 +119,15 @@ func TestConvert(t *testing.T) {
 	setupParam()
 	// TODO: old-style test, better update it with accept headers
 	var testChromeLink = map[string]string{
-		"http://127.0.0.1:3333/webp_server.jpg":                 "image/webp",
-		"http://127.0.0.1:3333/webp_server.bmp":                 "image/webp",
-		"http://127.0.0.1:3333/webp_server.png":                 "image/webp",
-		"http://127.0.0.1:3333/empty.jpg":                       "",
-		"http://127.0.0.1:3333/png.jpg":                         "image/webp",
-		"http://127.0.0.1:3333/12314.jpg":                       "",
-		"http://127.0.0.1:3333/dir1/inside.jpg":                 "image/webp",
-		"http://127.0.0.1:3333/%e5%a4%aa%e7%a5%9e%e5%95%a6.png": "image/webp",
-		"http://127.0.0.1:3333/太神啦.png":                         "image/webp",
+		"http://127.0.0.1:3333/webp_server.jpg": "image/webp",
+		"http://127.0.0.1:3333/webp_server.bmp": "image/webp",
+		"http://127.0.0.1:3333/webp_server.png": "image/webp",
+		"http://127.0.0.1:3333/empty.jpg":       "",
+		"http://127.0.0.1:3333/png.jpg":         "image/webp",
+		"http://127.0.0.1:3333/12314.jpg":       "",
+		"http://127.0.0.1:3333/dir1/inside.jpg": "image/webp",
+		// "http://127.0.0.1:3333/%e5%a4%aa%e7%a5%9e%e5%95%a6.png": "image/webp",
+		"http://127.0.0.1:3333/太神啦.png": "image/webp",
 	}
 
 	var testChromeAvifLink = map[string]string{
@@ -138,7 +138,7 @@ func TestConvert(t *testing.T) {
 		"http://127.0.0.1:3333/png.jpg":                         "image/avif",
 		"http://127.0.0.1:3333/12314.jpg":                       "",
 		"http://127.0.0.1:3333/dir1/inside.jpg":                 "image/avif",
-		"http://127.0.0.1:3333/%e5%a4%aa%e7%a5%9e%e5%95%a6.png": "image/avif",
+		// "http://127.0.0.1:3333/%e5%a4%aa%e7%a5%9e%e5%95%a6.png": "image/avif",
 		"http://127.0.0.1:3333/太神啦.png":                         "image/avif",
 	}
 
